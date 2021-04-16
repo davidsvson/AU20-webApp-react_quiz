26dff19efac8324cb808f822a9856dff726f310f
David Svensson <davidsvson@gmail.com>
2021-04-16 10:50:11 +0200

    showing right answer

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -5,16 +5,42 @@ import { useState } from "react";
 const Game = ({ answeredCorrectly }) => {
     const questions = getQuestions();
     const [currentQuestion, setQuerrentQuestion ] = useState(1)
-    
-    const question = questions[currentQuestion - 1 ]
+    const [selectedAnswer, setSelectedAnswer] = useState(null);
+    const [decided, setDecided] = useState(false);
+
+    const q = questions[currentQuestion - 1 ];
+
+    const handleDecided = () => {
+        setDecided(true);
+        if( selectedAnswer === q.correct) {
+            answeredCorrectly(); // report score back to App
+        }
+        setTimeout(() => {       
+            setQuerrentQuestion(currentQuestion + 1);
+            setSelectedAnswer(null);
+            setDecided(false);
+        }, 2000);
+    }
+
+    //let a = bool ? 'hej' : 'då'
+
+    const options = q.answers.map((answer, index) => (
+        <p className={"option" + ((decided && index === q.correct) ? ' correct' : '')}  key={answer}>
+            <label>
+                <input type="radio"
+                    onClick={() => setSelectedAnswer(index)} />
+                {answer}
+            </label>
+        </p>
+    ))
 
     return (
         <section className="game">
             <h2>Question {currentQuestion} of {questions.length} </h2>
-            <h3>Fråga? </h3>
+            <h3>{q.question}</h3>
+            {options}
 
-
-            <button onClick={answeredCorrectly}>Answer</button>
+            <button onClick={handleDecided}>Answer</button>
         </section>
     )
 
@@ -27,27 +53,27 @@ const Game = ({ answeredCorrectly }) => {
 function getQuestions() {
     return [
         {
-            question: 'När är det julafton',
+            question: 'När är det julafton?',
             answers: ['24 maj', '24 dec', '3 nov'],
             correct: 1
         },
         {
-            question: 'Vad heter David i mellannamn',
+            question: 'Vad heter David i mellannamn?',
             answers: ['sven', 'julia', 'anders'],
             correct: 2
         },
         {
-            question: 'Vad har vit för hex tal',
+            question: 'Vad har vit för hex tal?',
             answers: ['#000000', '#ffffff', '#15f36a'],
             correct: 1
         },
         {
-            question: 'vad sitter bredvid t på tangentbordet',
+            question: 'vad sitter bredvid t på tangentbordet?',
             answers: ['y', 'b', 'n'],
             correct: 0
         },
         {
-            question: 'vad är bäst',
+            question: 'vad är bäst?',
             answers: ['teams', 'discord', 'slack'],
             correct: 1
         }
